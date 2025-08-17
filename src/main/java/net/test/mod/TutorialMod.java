ce32a334004f044351e4573b040235a0f03b36f6
KeboUwU <sb316006@student.polsl.pl>
2025-08-17 19:02:25 +0200

    dziala?

--- src/main/java/net/test/mod/TutorialMod.java
+++ src/main/java/net/test/mod/TutorialMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//dziala?
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorial-mod";
 
